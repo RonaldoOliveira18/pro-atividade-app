8e8ba5ed0d2e4ccc83bcef6a37717d6bb47b62f6
Ronaldo Oliveira <you@example.com>
2022-09-23 10:14:10 +0100

    crud-react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import AtividadeForm from './components/AtividadeForm';
+import Atividade from './components/Atividade';
+
+let initialState =
+  [
+    {
+      "id": 1,
+      "descricao": "Primeira Atividade",
+      "prioridade": "1",
+      "titulo": "Título 1"
+    },
+    {
+      "id": 2,
+      "prioridade": "1",
+      "descricao": "Segunda Atividade",
+      "titulo": "Título 2"
+    },
+  ];
 
 function App() {
+  const [atividades, setAtividade] = useState(initialState);
+
+  function addAtividade(e) {
+    e.preventDefault();
+
+    const atividade = {
+      id: document.getElementById('id').value,
+      prioridade: document.getElementById('prioridade').value,
+      titulo: document.getElementById('titulo').value,
+      descricao: document.getElementById('descricao').value,
+    }
+
+    atividades.push(atividade);
+
+    setAtividade([...atividades]);
+  };
+
+  function deletarAtividade(id){
+    const atividadesFiltradas = atividades.filter(atividade => atividade.id !== id);
+    setAtividade([...atividadesFiltradas]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AtividadeForm
+          addAtividade={addAtividade}
+          atividades={atividades}
+      />
+      <div className="mt-3">
+        {atividades.map(ativ => (
+          <Atividade 
+              key={ativ.id} 
+              deletarAtividade={deletarAtividade}
+              ativ={ativ}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/AtividadeLista.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AtividadeLista() {
+  return (
+    <div>AtividadeLista</div>
+  )
+}
